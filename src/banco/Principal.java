e66f3b528df8f56f00f1190a7929def707c9fc8d
sabrinadiehl1 <sabrinadiehl1@users.noreply.github.com>
2018-10-23 20:21:38 -0200

    Commit inicial

---
+++ src/banco/Principal.java
@@ -0,0 +1,17 @@
+package banco;
+
+import banco.ui.InterfaceTexto;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		InterfaceTexto interfaceTexto = new InterfaceTexto();
+		
+		System.out.println("Agência Bancária");
+			
+		interfaceTexto.executa();
+
+	}
+	
+}
