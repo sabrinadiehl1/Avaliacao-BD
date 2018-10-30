920f82bdeaa18023e6e2db270659db33f89670e2
sabrinadiehl1 <sabrinadiehl1@users.noreply.github.com>
2018-10-30 20:31:20 -0200

    Commit correção

--- src/banco/dao/LivroDao.java
+++ src/banco/dao/LivroDao.java
@@ -13,8 +13,8 @@ import banco.modelo.Livro;
 
 public class LivroDao implements Dao<Livro> {
 	
-	private static final String GET_BY_ID = "SELECT * FROM livro NATURAL JOIN autor WHERE id = ?";
-	private static final String GET_ALL = "SELECT * FROM livro NATURAL JOIN autor";
+	private static final String GET_BY_ID = "SELECT * FROM livro l JOIN autor a on a.id = l.autor_id WHERE id = ?";
+	private static final String GET_ALL = "SELECT * FROM livro l JOIN autor a on a.id = l.autor_id";
 	private static final String INSERT = "INSERT INTO livro (titulo, anoPublicacao, editora, autor_id) "+ "VALUES (?, ?, ?, ?)";
 	private static final String UPDATE = "UPDATE livro SET titulo = ?, anoPublicacao = ?, editora = ?, "
 			+ "autor_id = ? WHERE id = ?";
